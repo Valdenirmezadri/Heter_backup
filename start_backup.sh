e90c76c5368367583a33f6e67b27e966239d09af
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2018-04-02 15:26:02 -0300

    Add files via upload

--- start_backup.sh
+++ start_backup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
 LOCAL_DIR="."
 . "$LOCAL_DIR/config.cfg"
+export LANG="$LOC.UTF-8"
 . "$LOCAL_DIR/sistema/config.cfg"
 . "$LOCAL_DIR/lang/$LOC.loc"
 
@@ -9,13 +13,29 @@ CFG="$LOCAL_DIR/config_backups"
 
 echo "$INICIO" > $LOG
 
-R=1
+#R=1
 ALERTERR=0
 
 # Carrega modulos
 for modulo in $MODULOS/mod_*; do
   [ -e "$modulo" ] || continue
   . $modulo
+done
+
+# Carrega arquivos cfg que rodam antes dos backups, limpeza por ex
+for cfgs in $CFG/before*.cfg; do
+  [ -e "$cfgs" ] || continue
+    . $cfgs
+    if [ -n "$TIPO" ]; then
+      function_exists $TIPO && R=0 || R=1
+      if [ $R -eq 0  ]; then
+        $TIPO
+      else
+        echo "Está faltando o módulo da função $TIPO"
+      fi
+    else
+      echo "Falta especificar o tipo de backup do $cfgs"
+    fi
 done
 
 # Carrega arquivos cfg de backup
